14a0c8c473966e447f419cbf23035344923ab63a
FlutLab.io <>
2021-11-30 08:03:50 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,34 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
+  Future fetchAllData() async {
+    final firestoreInstance = FirebaseFirestore.instance;
+    firestoreInstance.collection("users").get().then((querySnapshot) {
+      querySnapshot.docs.forEach((result) {
+        print(result.data());
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('DB'),
+      ),
+      body: FutureBuilder(
+        future: fetchAllData(),
+        builder: (context, AsyncSnapshot snapshot) {
+          return ListView.builder(
+            itemCount: snapshot.data.length,
+            itemBuilder: (ctx, index) {
+              DocumentSnapshot data = snapshot.data[index];
+              return ListTile(
+                title: Text(data['name']),
+              );
+            },
+          );
+        },
       ),
     );
   }
