833a240ac91bdda07b1560aced02ffc47c3c359b
FlutLab.io <>
2021-11-30 08:08:58 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: MyApp(),
